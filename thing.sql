077cae58e3ffbf577976cd17093f4c60698a3211
Knut Edvard Kjersem <knutekjersem@gmail.com>
2024-01-30 23:44:41 +0100

    Update thing.sql

--- thing.sql
+++ thing.sql
@@ -1,9 +1,17 @@
+# Data assumed to be credible and clean because of source, the companys booking system.
+# However upon first interaction to analyze by making a child per adult colum (Child/Adult) SQL threw a 0 / 0 erro
+# After inpsecting this problem, it showed that some bookings had no adults, and, in fact no guests at all. 
+# Futher (Confering with SME) on it showed that a vast majority came from bookings made from Portugal. And then one particular agent in Portugal lacked training in using the system.
+# They would some times alter bookings to remove all guests before cancelling it, only remove adults from a booking. SME, realised that training videos had only been shown to in house staff and not external agents.
+# Checked in completed bookings with zero guests, proved to be all linked to one sales rep, who would dupe all his stats to meat his goals, "agent 6". The findings have been passed along
 
-SELECT COUNT(*) as num_segment, market_segment, hotel
+  
+Select *, (stays_in_weekend_nights + stays_in_week_nights )as length_of_stay
 FROM
-(SELECT *,(CONCAT(arrival_date_year, arrival_date_month, arrival_date_day_of_month)) AS string_to_date 
-  FROM hote_world.bookings as off_peak
-  WHERE arrival_date_month = "January" 
+(SELECT *
+FROM hote_world.bookings
+where (adults + children + babies) != 0
+  AND arrival_date_month = "January" 
   OR  arrival_date_month = "February"
   OR  arrival_date_month = "March"
   OR  arrival_date_month = "April"
@@ -11,4 +19,5 @@ FROM
   OR  arrival_date_month = "October"
   OR  arrival_date_month = "November")
 
-GROUP BY market_segment, hotel
+  Where (stays_in_weekend_nights + stays_in_week_nights ) <= 21 
+  AND (stays_in_weekend_nights + stays_in_week_nights ) > 1 
