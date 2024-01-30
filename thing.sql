264f3052beb7d4288f6f000d5a859d6855be9424
Knut Edvard Kjersem <knutekjersem@gmail.com>
2024-01-31 00:16:25 +0100

    Update thing.sql

--- thing.sql
+++ thing.sql
@@ -3,10 +3,14 @@
 # After inpsecting this problem, it showed that some bookings had no adults, and, in fact no guests at all. 
 # Futher (Confering with SME) on it showed that a vast majority came from bookings made from Portugal. And then one particular agent in Portugal lacked training in using the system.
 # They would some times alter bookings to remove all guests before cancelling it, only remove adults from a booking. SME, realised that training videos had only been shown to in house staff and not external agents.
+  # where (adults + children + babies) != 0 addresses <- this issue
+
 # Checked in completed bookings with zero guests, proved to be all linked to one sales rep, who would dupe all his stats to meat his goals, "agent 6". The findings have been passed along
+ # AND (stays_in_weekend_nights + stays_in_week_nights ) > 1 <- adresses this issue
+
 
   
-Select *, (stays_in_weekend_nights + stays_in_week_nights )as length_of_stay
+Select AVG(adults) as avg_adult, AVG(Children) as avg_children, avg(lead_time) avg_lead_time, market_segment, distribution_channel
 FROM
 (SELECT *
 FROM hote_world.bookings
@@ -17,7 +21,10 @@ where (adults + children + babies) != 0
   OR  arrival_date_month = "April"
   OR arrival_date_month = "September" 
   OR  arrival_date_month = "October"
-  OR  arrival_date_month = "November")
-
-  Where (stays_in_weekend_nights + stays_in_week_nights ) <= 21 
+  OR  arrival_date_month = "November"
+  AND (stays_in_weekend_nights + stays_in_week_nights ) <= 21 
   AND (stays_in_weekend_nights + stays_in_week_nights ) > 1 
+  AND hotel = 'Resort Hotel')
+  GROUP BY market_segment, distribution_channel
+
+
